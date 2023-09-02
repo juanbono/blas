46104cdf4a28af7cfe8c9e2b3b499eb8a04fc035
Juan Bono <juanbono94@gmail.com>
2023-09-02 17:53:14 -0300

    add clap cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,13 +1,29 @@
 use core::RootDatabase;
 
-fn main() {
-    let db = RootDatabase::default();
-    let program = r#"
-    program HolaMundo 
-    end"#
-        .to_string();
+use clap::Parser;
 
+/// Arguments to the compiler CLI.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of file to compile
+    #[arg(short, long)]
+    file: String,
+}
+
+fn main() {
+    // parse command line arguments
+    let args = Args::parse();
+
+    // read the program from file
+    let program = std::fs::read_to_string(args.file).unwrap();
+
+    // create a new database where to store the program
+    let db = RootDatabase::default();
+
+    // compile the program
     let compiled_program = db.compile_string(program).unwrap();
+
     dbg!(compiled_program);
     dbg!(compiled_program.program_id(&db).text(&db));
 }
